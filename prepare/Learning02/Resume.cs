b13e760413ba50f51b9f04c0be933d9d27031ba5
Daniel Cross <danielcross2872@gmail.com>
2024-01-25 21:01:11 -0700

    Completed

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Resume{
+    public string _firstName;
+    public string _lastName;
+    public List<Job> _jobs = new List<Job>();
+    public void Display(){
+        Console.WriteLine($"Name: {_firstName} {_lastName}");
+        Console.WriteLine($"Jobs:");
+        foreach (Job job in _jobs){
+            job.Display();
+        }
+    }
+}
